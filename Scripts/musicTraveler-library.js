58cdaa7946eed99335a6703dda3a4862ad02bdeb
Francisco Valente <fco.valente.castro@gmail.com>
2012-03-12 14:30:38 -0600

    Backend Map Display Classes -Map -Marker -Route

--- Scripts/musicTraveler-library.js
+++ Scripts/musicTraveler-library.js
@@ -1,38 +1,53 @@
 /* Object Collection*/
-var Map = function(){
-	this.googleMap;
-	this.options;
-	this.markerCollection;
-	this.route.Collection;
+var Map = function(var objHTML,var mapOptions){
+
+	this.googleMap = new google.maps.Map(objHTML,mapOptions);
+	this.mapOptions = mapOptions;
+	this.markerCollection = new Array(0);
+	this.routeCollection = new Array(0);
 	
-	this.newMarker = function(){
-	
+	this.newMarker = function(var position){
+		this.markerCollection.push( new Marker( {  LatLng: position } , { content:"Test" } )  );
+		this.markerCollection[ this.markerCollection.length ].googleMarker.setMap(this.googleMap);	
 	}
 	
-	this.newRoute = function(){
-	
+	this.newRoute = function(var origin, var destination,var travelMode){
+		this.routeCollection.push( new Route( {  origin:origin, destination:destination, travelMode:travelMode}); );
+		this.routeCollection[ this.routeCollection.length ].directionsRenderer.setMap(this.googleMap);	
 	}
 				
 }
 
-var Marker = function(){
-	this.LatLng;
-	this.info;
-	this.title;
-	this.place;
-	this.icon;
-	this.infoWindow;
+var Marker = function(var markerOptions,var infoOptions){
+	/* Propiedades*/
+	this.googleMarker = 	new google.maps.Marker(markerOptions);
+	this.googleInfoWindow = new google.maps.InfoWindow(infoOptions);
+	
+	this.markerOptions = markerOptions;	
+	this.infoOptions = infoOptions;	
+		
+	
+	/* Metodos*/
+	this.onClick = function(){
+		this.googleInfoWindow.open(markerOptions.map);	
+	}		
+	
+	google.maps.event.addListener(this.googleMarker,"click",this.onClick);
+	
 }
 
-var Route = function(){
-	this.directions;
-	this.speed;
-	this.color;
-}
-
-var InfoWindow =  function(){
-	this.content;
-	this.offset;
+var Route = function(var routeOptions){
+	this.directionsService = new google.maps.DirectionsService();
+	
+	this.directionsRenderer = new google.maps.DirectionsRenderer();
+	
+	this.routeOptions = routeOptions;
+	
+	this.directionsService(routeOptions,function(var result,var status){
+			if (status == google.maps.DirectionsStatus.OK) {
+      		this.directionsRenderer.setDirections(result);
+    		}
+		});	
 }
 
 var MultimediaPlayer = function(){
